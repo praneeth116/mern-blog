97e2f1ce724999ce334a2c6c82db04ad8942aa8f
praneeth116 <praneeth.n116@gmail.com>
2024-07-04 18:31:35 +0530

    create user model

--- api/index.js
+++ api/index.js
@@ -4,9 +4,12 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(()=>{
+mongoose.
+connect(process.env.MONGO)
+.then(()=>{
     console.log("MongoDB is connected");
-}).catch((err)=>{
+})
+.catch((err)=>{
     console.log(err);
 });
 
