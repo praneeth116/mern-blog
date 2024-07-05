26dc777b37fc982e48f924b48a2e997271dd5a99
praneeth116 <praneeth.n116@gmail.com>
2024-07-05 12:48:14 +0530

    create sign up page ui

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import { Button, Label, TextInput } from 'flowbite-react';
+import { Link } from 'react-router-dom';
 
-function SignUp() {
+export default function SignUp() {
   return (
-    <div>Signup</div>
-  )
+    <div className='min-h-screen mt-20'>
+      <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row gap-5'>
+        <div className='flex-1'>
+          {/* Left */}
+          <Link to="/" className='whitespace-nowrap text-4xl 
+        font-semibold dark:text-white'>
+            <span className='px-2 py-1 bg-gradient-to-r from-indigo-500
+             via-purple-500 to-pink-500 rounded-lg text-white'>Praneeth's</span>
+            Blog
+          </Link>
+          <p className='text-sm mt-5'>
+            This is a demo project. You can sign up with your email and
+            password
+            or with Google
+          </p>
+        </div>
+        <div className='flex-1'>
+          {/* Right */}
+          <div className=''>
+            <form className='flex flex-col gap-4'>
+              <div>
+                <Label value="Your name"/>
+                <TextInput type='text' placeholder='Username' id='username'/>
+              </div>
+              <div>
+                <Label value="Your email"/>
+                <TextInput type='text' placeholder='name@company.com' id='email'/>
+              </div>
+              <div>
+                <Label value="Your password"/>
+                <TextInput type='text' placeholder='Password' id='password'/>
+              </div>
+              <Button gradientDuoTone='purpleToPink' type='submit'>
+              Sign Up
+              </Button>
+            </form>
+            <div className='flex gap-2 text-sm mt-5'>
+              <span>Have an account?</span>
+              <Link to="/sign-in" className='text-blue-500'>Sign In</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
-
-export default SignUp
